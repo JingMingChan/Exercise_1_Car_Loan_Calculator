0b3ba12a28391fed9f2b576d80330e45084ef00f
JingMingChan <prochan75103161@gmail.com>
2019-11-30 21:21:07 +0800

    Exercise 1 car loan calculator ver 2

--- app/src/main/java/com/example/exercise_1_car_loan_calculator/MainActivity.kt
+++ app/src/main/java/com/example/exercise_1_car_loan_calculator/MainActivity.kt
@@ -5,25 +5,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
-import android.app.Activity
 import android.content.Context
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.app.ComponentActivity.ExtraData
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import java.text.DecimalFormat
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var carPrice: EditText
     lateinit var downPayAmt: EditText
-    lateinit var LoanPeriod: EditText
-    lateinit var InterestRate: EditText
+    lateinit var loanPeriod: EditText
+    lateinit var interestRate: EditText
     lateinit var carloan: TextView
     lateinit var interest: TextView
     lateinit var monRepay: TextView
+
+    val formater = DecimalFormat("0.00")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         carPrice = findViewById(R.id.carPrice)
         downPayAmt = findViewById(R.id.downPayAmt)
-        LoanPeriod = findViewById(R.id.loanPeriod)
-        InterestRate = findViewById(R.id.interestRate)
+        loanPeriod = findViewById(R.id.loanPeriod)
+        interestRate = findViewById(R.id.interestRate)
 
         carloan = findViewById(R.id.carLoanVal)
         interest = findViewById(R.id.interestVal)
@@ -50,13 +49,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculate(view: View){
 
+        try {
 
-        carloan.text = ("%.2f".format(carPrice.text.toString().toDouble() - downPayAmt.text.toString().toDouble()).toDouble()).toString()
+            carloan.setText(formater.format(carPrice.text.toString().toDouble() - downPayAmt.text.toString().toDouble()))
+            interest.setText(formater.format(carloan.text.toString().toDouble() * interestRate.text.toString().toDouble() * loanPeriod.text.toString().toDouble()))
 
+            if((carloan.text.toString().toDouble() + interestRate.text.toString().toDouble()) == 0.toDouble()){
+                monRepay.setText(formater.format(0))
+            }else {
+                monRepay.setText(formater.format((carloan.text.toString().toDouble() + interestRate.text.toString().toDouble()) / loanPeriod.text.toString().toDouble() / 12.toDouble()))
+            }
 
-        interest.text = (carloan.text.toString().toDouble() * InterestRate.text.toString().toDouble() * LoanPeriod.text.toString().toDouble()).toString()
-
-        monRepay.text = ((carloan.text.toString().toDouble() + InterestRate.text.toString().toDouble()) / LoanPeriod.text.toString().toDouble() / 12.toDouble()).toString()
+        }catch (e:Exception){
+            carloan.setText(formater.format(0))
+            interest.setText(formater.format(0))
+            monRepay.setText(formater.format(0))
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
@@ -66,9 +74,8 @@ class MainActivity : AppCompatActivity() {
 
         carPrice.setText("")
         downPayAmt.setText("")
-        LoanPeriod.setText("")
-        InterestRate.setText("")
-
+        loanPeriod.setText("")
+        interestRate.setText("")
         carloan.setText("")
         interest.setText("")
         monRepay.setText("")
